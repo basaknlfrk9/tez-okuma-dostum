60e59e5d810873c26511200a913a7822170800c1
basaknlfrk9 <basak.nlfrk.94@gmail.com>
2025-12-16 19:10:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if "user" not in st.session_state:
 else:
     ad = st.session_state.user
     st.sidebar.success(f"Kullanıcı: {ad}")
-    st.title(f"📚 {ad} icin Okuma Dostum")
+    st.title(f"📚 {ad} 'in Okuma Dostu")
 
     if "OPENAI_API_KEY" not in st.secrets:
         st.error("Şifre Yok!")
@@ -112,4 +112,5 @@ else:
             {"role": "assistant", "content": cevap}
         )
         gecmis_kaydet(ad, st.session_state.messages)
+
 
